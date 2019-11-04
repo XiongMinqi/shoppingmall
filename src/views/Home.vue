ba7c6d91f4ae7184ea12c0b7b50c02a767b3c14b
XiongMinqi <1447259297@qq.com>
2019-11-04 09:16:00 +0800

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    Home
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "home",
-  components: {
-    HelloWorld
-  }
+  name: "Home",
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+  methods: {},
+  mounted() {},
+  created() {},
+  filters: {},
+  computed: {},
+  watch: {},
+  directives: {}
 };
 </script>
+
+<style scoped lang="scss"></style>
